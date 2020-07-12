445c6790b1d637eae354d20f5d812c8f9fc25168
Addin Gama Bertaqwa <punyagama@gmail.com>
2020-07-13 07:13:33 +0800

    Delete photo

--- app/src/components/atoms/profile-photo/profile-photo.tsx
+++ app/src/components/atoms/profile-photo/profile-photo.tsx
@@ -9,7 +9,7 @@ import {
 } from 'react-native';
 import {metrics} from 'src/themes';
 import FastImage from 'react-native-fast-image';
-import { Photo } from 'src/models';
+import {Photo} from 'src/models';
 
 interface ProfilePhotoProps {
   data: Photo;
@@ -19,6 +19,7 @@ interface ProfilePhotoProps {
   onPress?: () => void;
 }
 
+const CLOSE_IMAGE = require('./ic_close.png');
 export const ProfilePhoto = ({data, ...props}: ProfilePhotoProps) => {
   return (
     <View style={[root, props.containerStyle]}>
@@ -29,12 +30,15 @@ export const ProfilePhoto = ({data, ...props}: ProfilePhotoProps) => {
         style={[imageStyle, props.imageStyle]}
         resizeMode={FastImage.resizeMode.cover}
       />
+      <TouchableOpacity onPress={props.onDelete} style={closeContainerStyle}>
+        <Image source={CLOSE_IMAGE} style={closeImageStyle} />
+      </TouchableOpacity>
     </View>
   );
 };
 
 const containerWidth = metrics.screenWidth / 3.6;
-const containerHeight = containerWidth * 1.5
+const containerHeight = containerWidth * 1.5;
 const root: ViewStyle = {
   width: containerWidth,
   height: containerHeight,
@@ -44,5 +48,29 @@ const imageStyle: ImageStyle = {
   resizeMode: 'cover',
   width: containerWidth,
   height: containerHeight,
-  borderRadius: 15
+  borderRadius: 15,
+};
+
+const closeImageStyle: ImageStyle = {
+  width: 15,
+  height: 15,
+  resizeMode: 'contain',
+};
+
+const closeContainerStyle: ViewStyle = {
+  position: 'absolute',
+  bottom: -10,
+  right: -10,
+  backgroundColor: 'white',
+  padding: 10,
+  borderRadius: 30,
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 3,
+  },
+  shadowOpacity: 0.29,
+  shadowRadius: 4.65,
+
+  elevation: 7,
 };
--- app/src/screens/profile/profile-screen.tsx
+++ app/src/screens/profile/profile-screen.tsx
@@ -8,42 +8,61 @@ import {DEFAULT_API_CONFIG} from 'src/services/api/api-config';
 const MEMBER_ID = 1;
 
 export const ProfileScreen = () => {
-  let api: Api = null;
+  let api: Api = new Api(DEFAULT_API_CONFIG);
   const [member, setMember] = useState<Member>({});
   const [photos, setPhotos] = useState<Photo[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setup();
   }, []);
 
   const setup = async () => {
-    api = new Api();
-    await api.setup(DEFAULT_API_CONFIG);
+    await api.setup();
     await fetchMember();
     await fetchPhotos();
   };
 
   const fetchMember = async () => {
-    const response = await api.getMember(MEMBER_ID);
-    await setMember(response);
+    try {
+      setLoading(true);
+      const response = await api.getMember(MEMBER_ID);
+      console.log(response)
+      setMember(response);
+      fetchPhotos(response.id);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setLoading(false);
+    }
   };
 
-  const fetchPhotos = async () => {
-    const response: PhotoResponse = await api.getPhotos(member.id);
-    setPhotos(response);
+  const fetchPhotos = async (id: number) => {
+    if (id) {
+      const response: PhotoResponse = await api.getPhotos(id);
+      setPhotos(response);
+    }
   };
 
-  const renderItem = ({item}: {item: Photo}) => {
-    return <ProfilePhoto data={item} />;
+  const deletePhoto = (index: number) => {
+    let newPhotos = [...photos];
+    newPhotos.splice(index, 1);
+    setPhotos(newPhotos);
+  };
+
+  const renderItem = ({item, index}: {item: Photo; index: number}) => {
+    return <ProfilePhoto data={item} onDelete={() => deletePhoto(index)} />;
   };
   return (
     <SafeAreaView style={{flex: 1}}>
       <Grid
+        refreshing={loading}
+        onRefresh={setup}
         numColumns={3}
         data={photos}
         renderItem={renderItem}
         marginExternal={8}
-        marginInternal={8}
+        marginInternal={20}
         contentContainerStyle={{flexGrow: 1}}
       />
     </SafeAreaView>
